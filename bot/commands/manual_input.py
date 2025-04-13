1c3190b2db2320e15b92f0c5de530da19ec66ab0
UnoBaguette <icrossett9@gmail.com>
2025-04-12 22:26:03 -0700

    Bugfix for user currency saving

--- bot/commands/manual_input.py
+++ bot/commands/manual_input.py
@@ -8,11 +8,11 @@ def perform_manual_tribute_input(amount, user, month, current_totals_info):
     from_currency = get_currency_from_amount(amount)
     amount = float(re.sub("[^\d\.]", "", amount))
     try: 
-        user_currency = current_totals_info[str(user.id)]["Currency"]
+        user_currency = Currency._member_map_[current_totals_info[str(user.id)]["Currency"]]
     except KeyError:
         current_totals_info[str(user.id)] = {"Tributes": [], "Currency": "USD"}
         user_currency = Currency.USD
-    amount = convert_currency(amount, from_currency, current_totals_info)
+    amount = convert_currency(amount, from_currency, user_currency)
     try:
         current_totals_info[str(user.id)]["Tributes"][month-1] += amount
     except KeyError:
